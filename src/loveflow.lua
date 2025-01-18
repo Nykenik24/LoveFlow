be46a6ac336f37d81562f304c701d03b430b6c95
Nykenik24 <Nykenik24@proton.me>
2025-01-18 22:42:03 +0100

    Made 'src/loveflow.lua' and utils

---
+++ src/loveflow.lua
@@ -0,0 +1,22 @@
+local bus = require("src.nodes.bus")
+
+---@class LoveFlow
+local loveflow = {}
+
+---Create a new architecture with an EventBus.
+---@return LoveFlow.Architecture
+function loveflow.newArch()
+	---@class LoveFlow.Architecture
+	local arch = {
+		---@type LoveFlow.EventBus
+		bus = bus.new(),
+		---Update all that can be updated in the architecture.
+		---@param self LoveFlow.Architecture
+		updateAll = function(self)
+			self.bus:update()
+		end,
+	}
+	return arch
+end
+
+return loveflow
---
+++ src/utils.lua
@@ -0,0 +1,52 @@
+local function Alphabet()
+	return {
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+		"g",
+		"h",
+		"i",
+		"j",
+		"k",
+		"l",
+		"m",
+		"n",
+		"o",
+		"p",
+		"q",
+		"r",
+		"s",
+		"t",
+		"w",
+		"x",
+		"y",
+		"z",
+	}
+end
+
+---Generates a random 8-character ID
+---@return number
+function GenerateID()
+	local chars = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+	for _, letter in ipairs(Alphabet()) do
+		table.insert(chars, letter)
+		table.insert(chars, letter:upper())
+	end
+
+	local id = ""
+	for i = 1, 8 do
+		local random_char = chars[math.random(1, #chars)]
+		id = id .. tostring(random_char)
+	end
+	return id
+end
+
+---Remove and return last element of a table
+---@param t table
+---@return any
+function Pop(t)
+	return table.remove(t, #t)
+end
